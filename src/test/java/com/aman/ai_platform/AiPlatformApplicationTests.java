b766d1c1f7fc2f54fcf740496046f4187d5caf93
itzamanarora <amanarora99005@gmail.com>
2026-02-18 15:13:49 +0530

    Initial Commit

---
+++ src/test/java/com/aman/ai_platform/AiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.ai_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
